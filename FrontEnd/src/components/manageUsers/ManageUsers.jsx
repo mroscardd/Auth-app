26a9d573f1b1f4c7de3af6a78c4a509d7c815bc7
mroscardd <mroscardd@gmail.com>
2026-09-25 18:53:02 +0200

    users

--- FrontEnd/src/components/manageUsers/ManageUsers.jsx
+++ FrontEnd/src/components/manageUsers/ManageUsers.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import "./ManageUsers.css"
 const env = import.meta.env
 
 const url = `http://localhost:${env.VITE_PORT}/api/users`
@@ -6,6 +8,8 @@ const url = `http://localhost:${env.VITE_PORT}/api/users`
 export function ManageUsers() {
     const [users, setUsers] = useState([])
     const [auth, setAuth] = useState(false)
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetch(url, {
@@ -21,18 +25,25 @@ export function ManageUsers() {
                 })
     }, [])
 
+    const handleClick = (e) => {
+        e.prevent.default()
+        navigate("/")
+    }
+
     return (
         <table className="usersContainer">
             <tr>
                 <th>Username</th>
                 <th>Email</th>
                 <th>Role</th>
+            
             </tr>
             { auth && users.map(user => { return (
                 <tr key={user._id}>
                     <td>{user.username}</td>
                     <td>{user.email}</td>
                     <td>{user.role}</td>
+                    <td><button className="edit-btn" onClick={(e) => handleClick(e)}>Editar</button></td>
                 </tr>
                 )}
             )}
